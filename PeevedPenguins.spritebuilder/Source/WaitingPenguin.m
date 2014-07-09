3853079942ca9a9fef395760986c18bb54375dce
Nathan Taylor <nathan@nathantaylor.com>
2014-07-08 22:46:15 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.m
+//  PeevedPenguins
+//
+//  Created by Nathan Taylor on 7/8/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "WaitingPenguin.h"
+
+@implementation WaitingPenguin
+
+@end
